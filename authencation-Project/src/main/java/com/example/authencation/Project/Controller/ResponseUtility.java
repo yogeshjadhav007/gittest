1b843aeecb73573ba35d1ce4de6323b02d031d50
yogeshjadhav <yogeshsj@lentra.ai>
2023-01-04 17:54:42 +0530

    sampleWebProject..........adding file

---
+++ authencation-Project/src/main/java/com/example/authencation/Project/Controller/ResponseUtility.java
@@ -0,0 +1,22 @@
+package com.example.authencation.Project.Controller;
+
+import org.json.JSONObject;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ResponseUtility {
+
+    public String errorResponse(String message) {
+        JSONObject errorResponse = new JSONObject();
+        errorResponse.put("status", "error");
+        errorResponse.put("message", message);
+        return errorResponse.toString();
+    }
+
+    public String successResponse(String message) {
+        JSONObject successResponse = new JSONObject();
+        successResponse.put("status", "success");
+        successResponse.put("message", message);
+        return successResponse.toString();
+    }
+}
